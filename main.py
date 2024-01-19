d677051416d70717e1e221d0e6257da0f91ea610
Simran <105905532+Simran807@users.noreply.github.com>
2024-01-19 23:04:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,26 +29,6 @@ map[v -1][h-1]="X"
 
 print(f"{row1}\n{row2}\n{row3}")'''
 
-import random
-rock= "✊"
-paper = "✋"
-scissor="✌🏼"
-game_image=[rock,paper,scissor]
-c = int(input("What do you choose?Type 0 for rock,1 for paper or 2 for Scissors"))
-print(game_image [c])
-   
-c_c = random.randint(0,2)
-print(f"computer choice{c_c}")
-print(game_image[c_c])
-if c == 0 and c_c ==2 :
-    print("you win")
-elif( c>=3 or c<0):
-    print("You typed invalid number you lose!")
-elif(c_c ==0 and c ==2):
-    print("You lose")        
-elif c_c > c:
-    print("you lose")
-elif c_c == c:
-    print ("it's a draw")    
+
 
 
